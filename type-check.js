afdd854891119272cd32ab6ef667d4bd47cec5e1
huqi <huqi1008301@163.com>
2019-09-16 14:26:06 +0800

    fix package.json

--- type-check.js
+++ type-check.js
@@ -21,21 +21,13 @@ shallowequal("a", "b", void 0);
 // this is legal because function returns void 0
 shallowequal("a", "b", () => {});
 
-shallowequal(
-  "a",
-  "b",
-  (): boolean => {
-    return true;
-  }
-);
+shallowequal("a", "b", (): boolean => {
+  return true;
+});
 
-shallowequal(
-  "a",
-  "b",
-  (x, y, z: ?string): boolean => {
-    return true;
-  }
-);
+shallowequal("a", "b", (x, y, z: ?string): boolean => {
+  return true;
+});
 
 shallowequal(
   "a",
